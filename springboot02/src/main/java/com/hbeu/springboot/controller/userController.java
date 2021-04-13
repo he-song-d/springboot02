9115d8e8adf929b42d3b3025598caae42bc559e6
hesong <heart0123@163.com>
2021-04-13 20:04:07 +0800

    dy

---
+++ springboot02/src/main/java/com/hbeu/springboot/controller/userController.java
@@ -0,0 +1,8 @@
+package com.hbeu.springboot.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class userController {
+}
---
+++ springboot02/src/test/java/com/hbeu/springboot/Springboot02ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hbeu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot02ApplicationTests {
+
+    @Test
+   public void contextLoads() {
+        System.out.println("--------------");
+        System.out.println("11111111111111111111");
+    }
+
+}
